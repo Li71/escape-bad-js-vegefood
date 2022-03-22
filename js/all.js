848c7575a929a27cbaf70fcc4b9b5a2f98696e8b
Li71 <48586847+Li71@users.noreply.github.com>
2022-03-22 21:05:00 +0800

    HW

--- js/all.js
+++ js/all.js
@@ -5,24 +5,36 @@ var data;
 axios.get(url)
  .then(function(res){
   data=res.data.filter(a=>a.作物名稱)
+  
+  // console.log(data)
+  
+  console.log(data[0].作物名稱)
+  
   // TODO: 之後拆成 renderData 函式
   var str=""
+  
   data.forEach((b,index)=>{
-    // TODO: 改成 ES6 的 Template Literals (字面字串符)
-    var content="<tr><td>" + b.作物名稱 
-    +"</td><td>" + b.市場名稱
-    +"</td><td>" + b.上價
-    +"</td><td>" + b.中價
-    +"</td><td>" + b.下價
-    +"</td><td>" + b.平均價
-    +"</td><td>" + b.交易量
-    +"</td></tr>"
+  //   // TODO: 改成 ES6 的 Template Literals (字面字串符)
+    var content=`
+    <tr>
+      <td>${b.作物名稱}</td>
+      <td>${b.市場名稱}</td>
+      <td>${b.上價}</td>
+      <td>${b.中價}</td>
+      <td>${b.下價}</td>
+      <td>${b.平均價}</td>
+      <td>${b.交易量}</td>
+    </tr>
+    `
     str+=content
   })
+  
   table.innerHTML=str
 })
 
+
 var table=document.querySelector(".table-content")
+
 var showData=[]
 
 var category=""
@@ -31,6 +43,8 @@ var filter=document.querySelector(".filter")
 filter.addEventListener("click",filterCategory)
 
 function filterCategory(e){
+  console.log(e.target)
+  
   if(e.target.nodeName=="BUTTON"){
     category=e.target.dataset.category
     showData=data.filter((i)=>{
@@ -39,14 +53,17 @@ function filterCategory(e){
     // TODO: 之後拆成 renderData 函式
     var str=""
     showData.forEach((i,index)=>{
-      var content="<tr><td>" + i.作物名稱 
-      +"</td><td>" + i.市場名稱
-      +"</td><td>" + i.上價
-      +"</td><td>" + i.中價
-      +"</td><td>" + i.下價
-      +"</td><td>" + i.平均價
-      +"</td><td>" + i.交易量
-      +"</td></tr>"
+      var content=`
+      <tr>
+        <td>${i.作物名稱}</td>
+        <td>${i.市場名稱}</td>
+        <td>${i.上價}</td>
+        <td>${i.中價}</td>
+        <td>${i.下價}</td>
+        <td>${i.平均價}</td>
+        <td>${i.交易量}</td>
+      </tr>
+    `
       str+=content
     })
     table.innerHTML=str
